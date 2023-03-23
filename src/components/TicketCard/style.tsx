9b310204836a8332d44ae778f5c68cf02446bf7a
Francine Hahn <fran_hahn@hotmail.com>
2023-03-23 18:14:12 -0300

    feat: I made the responsiveness of the tickets page

--- src/components/TicketCard/style.tsx
+++ src/components/TicketCard/style.tsx
@@ -3,10 +3,17 @@ import styled from "styled-components"
 export const TicketSection = styled.div `
     display: flex;
     flex-direction: column;
-    padding: 2.5vh 1vw 3.5vh 1vw;
+    padding: 20px 20px 40px 20px;
     background-color: white;
     border-radius: 5px;
-    max-width: 18vw;
+    @media screen and (min-width: 800px) {
+        min-width: 270px; 
+        max-width: 280px;    
+    }
+    @media screen and (max-width: 800px) {
+        min-width: 290px; 
+        max-width: 300px;    
+    }
 
     img {
         width: 100%;
@@ -14,7 +21,6 @@ export const TicketSection = styled.div `
     }
 
     h3 {
-        font-size: 24px;
         color: black;
         background-color: white;
         margin: 2vh 0 1vh 0;
