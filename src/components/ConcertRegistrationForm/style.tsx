3d0a265c420b029f5bce6c17439788ad755c266a
Francine Hahn <fran_hahn@hotmail.com>
2023-03-28 22:17:27 -0300

    fix: I added a width to the axios error messages in the signup page and the forms components

--- src/components/ConcertRegistrationForm/style.tsx
+++ src/components/ConcertRegistrationForm/style.tsx
@@ -28,6 +28,22 @@ export const FormSection = styled.form `
         color: red;
         font-size: 14px;
         text-align: center;
+
+        @media screen and (min-width: 1200px) {
+            width: 30vw;
+        }
+        @media screen and (min-width: 1000px) and (max-width: 1200px) {
+            width: 35vw;
+        }
+        @media screen and (min-width: 800px) and (max-width: 1000px) {
+            width: 45vw;
+        }
+        @media screen and (min-width: 600px) and (max-width: 800px) {
+            width: 60vw;
+        }
+        @media screen and (max-width: 600px) {
+            width: 80vw;
+        }
     }
 
     #successMessage {
--- src/pages/Signup/style.tsx
+++ src/pages/Signup/style.tsx
@@ -57,6 +57,22 @@ export const SignupSection = styled.section `
     #axiosError {
         color: red;
         font-size: 16px;
+
+        @media screen and (min-width: 1200px) {
+            width: 30vw;
+        }
+        @media screen and (min-width: 1000px) and (max-width: 1200px) {
+            width: 35vw;
+        }
+        @media screen and (min-width: 800px) and (max-width: 1000px) {
+            width: 45vw;
+        }
+        @media screen and (min-width: 600px) and (max-width: 800px) {
+            width: 60vw;
+        }
+        @media screen and (max-width: 600px) {
+            width: 80vw;
+        }
     }
 `
 
