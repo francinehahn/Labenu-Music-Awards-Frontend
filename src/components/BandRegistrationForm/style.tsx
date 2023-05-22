344fc2150a8ad7abcb1e0fd16ac0182ced3ec607
Francine Hahn <fran_hahn@hotmail.com>
2023-05-22 14:13:05 -0300

    feat: I edited the style of the band card and the band registration form component

--- src/components/BandRegistrationForm/style.tsx
+++ src/components/BandRegistrationForm/style.tsx
@@ -5,7 +5,7 @@ export const FormSection = styled.form `
     div {
         p {
             color: red;
-            font-size: 14px;
+            font-size: .875rem;
             @media screen and (min-width: 1200px) {
                 width: 30vw;
             }
@@ -23,32 +23,32 @@ export const FormSection = styled.form `
             }
         }
     }
+`
 
-    #error {
-        color: red;
-        font-size: 14px;
-        text-align: center;
+export const AxiosError = styled.p `
+    color: red;
+    font-size: .875rem;
+    text-align: center;
 
-        @media screen and (min-width: 1200px) {
-            width: 30vw;
-        }
-        @media screen and (min-width: 1000px) and (max-width: 1200px) {
-            width: 35vw;
-        }
-        @media screen and (min-width: 800px) and (max-width: 1000px) {
-            width: 45vw;
-        }
-        @media screen and (min-width: 600px) and (max-width: 800px) {
-            width: 60vw;
-        }
-        @media screen and (max-width: 600px) {
-            width: 80vw;
-        }
+    @media screen and (min-width: 1200px) {
+        width: 30vw;
     }
-
-    #successMessage {
-        color: green;
-        font-size: 16px;
-        text-align: center;
+    @media screen and (min-width: 1000px) and (max-width: 1200px) {
+        width: 35vw;
     }
+    @media screen and (min-width: 800px) and (max-width: 1000px) {
+        width: 45vw;
+    }
+    @media screen and (min-width: 600px) and (max-width: 800px) {
+        width: 60vw;
+    }
+    @media screen and (max-width: 600px) {
+        width: 80vw;
+    }
+`
+
+export const SuccessMessage = styled.p `
+    color: green;
+    font-size: 1rem;
+    text-align: center;
 `
