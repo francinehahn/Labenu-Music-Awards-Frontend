dce0b4e78f56ccfb861ead4b9878d58c47d808ef
Francine Hahn <fran_hahn@hotmail.com>
2023-05-22 15:19:26 -0300

    feat: I edited the style of the footer and the header

--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 
-
 export const FooterSection = styled.footer `
     background-color: black;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 4vh 4vw;
+    padding: 1.5rem 2rem;
     border-top: 1px solid var(--light-grey);
 
     p {
         color: white;
-        font-size: 14px;
+        font-size: 0.875rem;
         text-align: center;
     }
 `
